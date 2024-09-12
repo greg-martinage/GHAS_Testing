d200410ff048b5f063445aa25623bb3de927346b
greg-martinage <greg.martinage@deltadatasoft.com>
2024-09-11 22:37:49 -0400

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ShellApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Shell Application!");
+        }
+    }
+}
